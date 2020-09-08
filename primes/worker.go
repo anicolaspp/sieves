5457da2b52dcf0b6a760e94572e1cf9afa1e9668
Nicolas A Perez <anicolaspp@gmail.com>
2020-09-08 14:37:20 -0400

    some comments

--- primes/worker.go
+++ primes/worker.go
@@ -17,26 +17,28 @@ type workerData struct {
 // If the partition has been exhausted, it sends -1 to the master
 func worker(wid int, size int, data *workerData) {
 
-	for i := wid*size + 1; i < wid*size+size+1; i++ {
-		data.data = append(data.data, i)
-	}
+	initPartition(wid, size, data)
 
 	for {
 		printMsg(wid, data.data)
+		// gather next value to be filtered from master
 		next := <-data.In
 		printMsg(wid, fmt.Sprintf("Next filter: %v", next))
 
+		// check if termination signal has been received
 		if next == -1 {
 			data.result <- data.data
 			return
 		}
 
+		// filter partition using next value
 		for i, v := range data.data {
 			if v != next && v%next == 0 {
 				data.data[i] = -1
 			}
 		}
 
+		// select the next local min
 		hastNext := false
 		for _, v := range data.data {
 			if v > next {
@@ -46,11 +48,19 @@ func worker(wid int, size int, data *workerData) {
 			}
 		}
 
+		// if there is not next local min, send signal of partition completed
 		if !hastNext {
 			data.out <- -1
 		}
 	}
 
+}
+
+//initPartition initializes worker partition
+func initPartition(wid int, size int, data *workerData) {
+	for i := wid*size + 1; i < wid*size+size+1; i++ {
+		data.data = append(data.data, i)
+	}
 }
 
 func printMsg(wid int, value interface{}) {
